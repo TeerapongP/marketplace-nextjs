5422527fdbeed0e34d8a53917f64b3498f5280a1
TeerapongP <thirapongp7@gmail.com>
2024-08-13 23:09:45 +0700

    first commit

---
+++ components/button.js
@@ -0,0 +1,10 @@
+// components/button.js
+import { Button } from '@mui/material';
+
+export default function CustomButton() {
+  return (
+    <Button variant="contained" color="primary">
+      hello world
+    </Button>
+  );
+}
