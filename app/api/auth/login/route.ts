72163ac803d9cfb9331449817e73240a4ae0e08d
TeerapongP <thirapongp7@gmail.com>
2024-08-24 22:38:56 +0700

    remove auth login

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,25 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import { prisma } from '../../../../lib/prisma';
-
-// Ensure JWT_SECRET is defined
-const JWT_SECRET = process.env.JWT_SECRET || "";
-if (!JWT_SECRET) {
-  throw new Error('JWT_SECRET environment variable is not defined');
-}
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const { username, password, roleId } = body;
+    const { username, roleId } = body;
 
-    if (!username || !password || !roleId) {
-      return NextResponse.json({ message: 'Username, password, and role are required' }, { status: 400 });
+    if (!username || !roleId) {
+      return NextResponse.json({ message: 'Username and role are required' }, { status: 400 });
     }
 
     const user = await prisma.user.findUnique({
-      where: { userName: username,roleId },
+      where: { userName: username, roleId },
       select: {
         userId: true,
         userName: true,
@@ -29,7 +21,6 @@ export async function POST(req: NextRequest) {
         phoneNumber: true,
         address: true,
         userImage: true,
-        password: true, // Include password to verify it
         roleId: true // Include roleId
       }
     });
@@ -38,29 +29,9 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: 'User not found' }, { status: 404 });
     }
 
-    // Compare provided password with stored hashed password
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
-    }
-
-    // Check if provided role matches user's role
-    if (roleId !== user.roleId) {
-      return NextResponse.json({ message: 'Invalid role' }, { status: 403 });
-    }
-
-    // Generate JWT
-    const token = jwt.sign(
-      { userId: user.userId, username: user.userName, roleId: user.roleId },
-      JWT_SECRET,
-      { expiresIn: '1h' }
-    );
-
-    return NextResponse.json({ message: 'Login successful', token, userId: user.userId, userName: username }, { status: 200 });
+    return NextResponse.json({ message: 'User found', userId: user.userId, userName: username }, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
   }
 }
- 
