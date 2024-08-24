cd04f906f1b276868b5d3f9c1fe9e4c1afd09737
TeerapongP <thirapongp7@gmail.com>
2024-08-24 13:57:34 +0700

    Add client-side login page with form handling and redirection

---
+++ app/api/role/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../lib/prisma"; // Adjust the path as necessary
+
+export async function GET(req: NextRequest) {
+  try {
+    const roles = await prisma.role.findMany({
+      select: {
+        roleId: true,
+        roleName: true,
+      },
+    });
+
+    // Return the list of roles
+    return NextResponse.json(roles, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+}
